747c040f26179aa2ca5cb545ca75f99c0419ad8f
omireles <osar8405@gmail.com>
2023-08-21 09:02:39 -0600

    Cambio de repositorio y creación de nuevo proyecto gifs

---
+++ Angular/03-gifs-app/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsModule } from '../gifs/gifs.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GifsModule,
+    SharedModule
+  ]
+})
+export class SharedModule { }
